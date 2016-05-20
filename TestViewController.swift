7fc573724711fc39491de198176dc6b10d26fa63
Jerome1210 <b99705008@ntu.edu.tw>
2016-05-20 14:19:16 +0800

    feat: add a fail test intentionally

---
+++ TestViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TestViewController.swift
+//  CITesting
+//
+//  Created by Lee on 5/20/16.
+//  Copyright © 2016 Lee. All rights reserved.
+//
+
+import XCTest
+
+class TestViewController: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(1 != 1)
+    }
+    
+    
+}
